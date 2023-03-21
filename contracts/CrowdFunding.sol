416dcc5b3e361feeb3dffc138f32542e00cb9b06
lathi500 <Bhavin>
2023-03-21 15:16:34 +0530

    Applied required changes V2

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -5,44 +5,54 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Crowdfunding {
 
-    IERC20 myToken;
 
     /**
      * @dev emitted on project creation. User can get created projectId.
-     */
+    */
+    event projectCreated(bytes32 projectId, address projectOwner);
 
-    event projectCreated( bytes32 projectId, address projectOwner );
-    event fundProvided( uint256 userFundedAmount, address fundProvider );
-    event fundClaimed(   uint256 totalClaimedFund, address projectOwner );
-    event fundRefunded(  uint256 reFundedAmount, address user );
-     
+    /**
+     * @dev emitted when user has successfully provided fund to project
+    */
+    event fundProvided(uint256 userFundedAmount, address fundProvider);
+
+    /**
+     * @dev emitted when project owner has suceessfully claimed fund
+     */
+    event fundClaimed(uint256 totalClaimedFund, address projectOwner);
+
+    /**
+     * emitted when user refunded whith provided fund on project.
+     */
+    event fundRefunded(uint256 reFundedAmount, address user);
+
     address public owner;
-   
-     struct projectData
-     {
+
+    struct projectData {
         address projectCreator;
         uint startingTime;
         uint endTime;
         uint fundingGoal;
         uint totalRecievedFund;
-     }
+    }
 
-     mapping ( bytes32 => projectData ) public _projectData;
-     mapping ( bytes32 => mapping(address => uint)) public customrFundedAmount;
+    IERC20 myToken;
 
-     /**
-      * @param _myToken Platform token address
-      */
+    mapping(bytes32 => projectData) public _projectData;
+    mapping(bytes32 => mapping(address => uint)) public customrFundedAmount;
 
-    constructor(address _myToken) 
-    {
+    /**
+     * @param _myToken Platform token address
+     */
+
+    constructor(address _myToken) {
         myToken = IERC20(_myToken);
         owner = msg.sender;
     }
 
     modifier isOwner() {
-           require(msg.sender == owner,"Caller Is Not Owner");
-           _; 
+        require(msg.sender == owner, "Caller Is Not Owner");
+        _;
     }
 
     /**
@@ -52,41 +62,58 @@ contract Crowdfunding {
      * @return bool project creation status
      */
 
-    function startProject( uint _fundingGoal, uint _endTime  ) external isOwner returns(bool) 
+    function startProject(uint _fundingGoal, uint _endTime)
+        external
+        isOwner
+        returns (bool)
     {
-            require( _endTime > block.timestamp," End time must be grater than start time");
-            require( _fundingGoal > 0," Must have required goal fund ");
+        require(
+            _endTime > block.timestamp,
+            " End time must be grater than start time"
+        );
+        require(_fundingGoal > 0, " Must have required goal fund ");
 
-            bytes32 projectId = keccak256( abi.encodePacked(
+        bytes32 projectId = keccak256(
+            abi.encodePacked(
                 msg.sender,
                 block.timestamp,
                 _endTime,
                 _fundingGoal
-            ));
+            )
+        );
 
-            _projectData[projectId].projectCreator = msg.sender;
-            _projectData[projectId].startingTime = block.timestamp;
-            _projectData[projectId].endTime = _endTime;
-            _projectData[projectId].fundingGoal = _fundingGoal;
+        _projectData[projectId].projectCreator = msg.sender;
+        _projectData[projectId].startingTime = block.timestamp;
+        _projectData[projectId].endTime = _endTime;
+        _projectData[projectId].fundingGoal = _fundingGoal;
 
-            emit projectCreated(projectId, msg.sender); // User can get project Id thorough emited event
-            return true;
-    }        
-    
-     /**
+        emit projectCreated(projectId, msg.sender); // User can get project Id thorough emited event
+        return true;
+    }
+
+    /**
      * @dev user can fund on project using projectId.
-     * @param projectId  projectId on wich user want to provide fund. 
+     * @param projectId  projectId on wich user want to provide fund.
      * @param amounForFund fund amount user want to provide for given project id.
      */
 
-    function fundProject( bytes32 projectId, uint amounForFund ) external
-    {
-         require(_projectData[projectId].endTime > block.timestamp,"Time is over for accepting fund on given project");
-         require( msg.sender != owner, "Owner can not fund an project");
-         require(_projectData[projectId].fundingGoal > amounForFund,"Over loaded required fund amount" );
-         require((_projectData[projectId].totalRecievedFund + amounForFund) <= _projectData[projectId].fundingGoal
-         && _projectData[projectId].totalRecievedFund <= _projectData[projectId].fundingGoal,
-         "No more fund required for project");
+    function fundProject(bytes32 projectId, uint amounForFund) external {
+        require(
+            _projectData[projectId].endTime > block.timestamp,
+            "Time is over for accepting fund on given project"
+        );
+        require(msg.sender != owner, "Owner can not fund an project");
+        require(
+            _projectData[projectId].fundingGoal > amounForFund,
+            "Over loaded required fund amount"
+        );
+        require(
+            (_projectData[projectId].totalRecievedFund + amounForFund) <=
+                _projectData[projectId].fundingGoal &&
+                _projectData[projectId].totalRecievedFund <=
+                _projectData[projectId].fundingGoal,
+            "No more fund required for project"
+        );
 
         myToken.approve(address(this), amounForFund);
         myToken.transferFrom(msg.sender, address(this), amounForFund);
@@ -94,21 +121,32 @@ contract Crowdfunding {
         customrFundedAmount[projectId][msg.sender] = amounForFund;
         _projectData[projectId].totalRecievedFund += amounForFund;
 
-        emit fundProvided( amounForFund, msg.sender );
+        emit fundProvided(amounForFund, msg.sender);
     }
 
     /**
      * @dev Project owner can claim fund using project id( Note: project fundraise time must be completed )
      * @param projectId Id of project
      */
-    
-    function claimFunds( bytes32 projectId ) public isOwner {
-        require(_projectData[projectId].endTime < block.timestamp,"Can not claim befor project ending");
-        require(_projectData[projectId].fundingGoal == _projectData[projectId].totalRecievedFund,"Funding Goal is not reached");
 
-        myToken.approve(msg.sender,_projectData[projectId].fundingGoal);
-        myToken.transferFrom(address(this), msg.sender, _projectData[projectId].fundingGoal);
-        emit fundClaimed( _projectData[projectId].fundingGoal, msg.sender );
+    function claimFunds(bytes32 projectId) public isOwner {
+        require(
+            _projectData[projectId].endTime < block.timestamp,
+            "Can not claim befor project ending"
+        );
+        require(
+            _projectData[projectId].fundingGoal ==
+                _projectData[projectId].totalRecievedFund,
+            "Funding Goal is not reached"
+        );
+
+        myToken.approve(msg.sender, _projectData[projectId].fundingGoal);
+        myToken.transferFrom(
+            address(this),
+            msg.sender,
+            _projectData[projectId].fundingGoal
+        );
+        emit fundClaimed(_projectData[projectId].fundingGoal, msg.sender);
     }
 
     /**
@@ -117,17 +155,24 @@ contract Crowdfunding {
      */
 
     function getRefund(bytes32 projectId) public {
-        require(_projectData[projectId].totalRecievedFund <  _projectData[projectId].fundingGoal,"funding goal not met");  // funding goal not met
-        require(block.timestamp >= _projectData[projectId].endTime,"project time must be ended");               // in the withdrawal period
+        require(
+            _projectData[projectId].totalRecievedFund <
+                _projectData[projectId].fundingGoal,
+            "funding goal not met"
+        ); // funding goal not met
+        require(
+            block.timestamp >= _projectData[projectId].endTime,
+            "project time must be ended"
+        ); // in the withdrawal period
 
         uint256 amount = customrFundedAmount[projectId][msg.sender];
 
-        require( amount > 0, "User does not have staked any amount");
+        require(amount > 0, "User does not have staked any amount");
 
         customrFundedAmount[projectId][msg.sender] = 0;
-        myToken.approve(msg.sender,amount);
+        myToken.approve(msg.sender, amount);
         myToken.transferFrom(address(this), msg.sender, amount);
-        emit fundRefunded( amount, msg.sender );
+        emit fundRefunded(amount, msg.sender);
     }
 }
 
@@ -135,5 +180,3 @@ contract Crowdfunding {
 // User1: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 // User2: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
 // User3: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
-
-
